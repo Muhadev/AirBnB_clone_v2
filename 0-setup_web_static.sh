20e660c8d760b2cdb85a89b48e3205b1fbb1d1fb
Muhadev <muhammedfayemi@gmail.com>
2024-03-12 07:37:58 -0700

    task is fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,36 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# script that sets up web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-# Install Nginx if not already installed
-if ! command -v nginx > /dev/null; then
-    sudo apt-get -y update
-    sudo apt-get -y install nginx
-fi
-
-# Create necessary directories
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-
-# Create a fake HTML file
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
 sudo echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-# Create symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-# Set ownership to ubuntu user and group recursively
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update Nginx configuration
-nginx_config="/etc/nginx/sites-available/default"
-nginx_alias="location /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}\n"
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-if ! grep -q "location /hbnb_static/" "$nginx_config"; then
-    sudo sed -i "/server_name _;/ a $nginx_alias" "$nginx_config"
-fi
-
-# Restart Nginx
 sudo service nginx restart
