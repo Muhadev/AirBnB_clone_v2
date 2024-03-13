943c2d25105228c9701befb96bd768e24e676fba
Muhadev <muhammedfayemi@gmail.com>
2024-03-13 02:28:12 -0700

    task is fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,46 +1,30 @@
 #!/usr/bin/python3
-"""Distributes an archive to web servers"""
-import os.path
-from fabric.api import env, put, run
+"""
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
+"""
 
-
-env.hosts = ["54.236.46.198", "52.91.123.123"]
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['54.89.109.87', '100.25.190.21']
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server
-    Args:
-        archive_path (str): the path of the archive to distribute.
-    Returns:
-        If the file doesn't exist return False else True.
-    """
-    if os.path.isfile(archive_path) is False:
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
-
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
         return False
-    if run("sudo rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("sudo mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("sudo rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("sudo mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("sudo rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("sudo rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-        return False
-    return True
